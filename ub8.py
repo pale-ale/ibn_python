e1a678a931093975f780cee68dc02c7e74137cbc
pale-ale <alex.baucke@tutanota.com>
2021-06-16 23:00:22 +0200

    add romberg

--- ub8.py
+++ ub8.py
@@ -1,60 +1,49 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
-import numerikLib
 
-n = 10
 l = 0
 r = 1
-
-ALPHA = 1
-KAPPA = 4
 
 def f(x):
     return math.sin(math.pi*x)
 
+def F(x):
+    return -math.cos(math.pi*x)/math.pi
+
 def trapez_integral(func, l:float, r:float, count:int):
-    h = (r-l)/(count-1)
+    h = (r-l)/(count)
     ysum = 0
-    xvals = np.linspace(l, r, count)
+    xvals = np.linspace(l, r, count+1)
     yvals = [func(x) for x in xvals]
     for i in range(0,len(xvals)-1):
         yl = yvals[i]
         yr = yvals[i+1]
-        ysum += (yl + yr)*.5
-    return ysum * h
+        ysum += yl + yr
+    return ysum * h * .5
 
-print(trapez_integral(f, l, r, n))
-exit()
+def romberg(func, l:float, r:float, count:int):
+    result = []
+    h = (r-l)
+    result.append([(h/2.0)*(func(l)+func(r))])
+    for i in range(1,count+1):
+        h = h/2.
+        sum = 0
+        for k in range(1,2**i ,2):
+            sum = sum + func(l+k*h)
+        rowi = [0.5*result[i-1][0] + sum*h]
+        for j in range(1,i+1):
+            rij = rowi[j-1] + (rowi[j-1]-result[i-1][j-1])/(4.**j-1.)
+            rowi.append(rij)
+        result.append(rowi)
+    return result
 
-for i in range(7,n):
-    interppointsx = np.linspace(a,b,i+1)
-    interppointsy1 = [func1(x) for x in interppointsx]
-    interppointsy2 = [func2(x) for x in interppointsx]
-    intervalsize = [(interppointsx[i+1] - interppointsx[i]) for i in range(len(interppointsx)-1)]
+ideal = F(1) - F(0)
+print("Ideal:", ideal)
 
-    yvals1 = [func1(x) for x in xvals]
-    yvals2 = [func2(x) for x in xvals]
-
-    moments1 = numerikLib.solve_moments2(intervalsize, interppointsy1, 0, 0)
-    moments2 = numerikLib.solve_moments2(intervalsize, interppointsy2, 1, 1)
-    interppolys1 = [numerikLib.s_i(moments1, interppointsy1, intervalsize, k) for k in range(i)]
-    interppolys2 = [numerikLib.s_i(moments2, interppointsy2, intervalsize, k) for k in range(i)]
-
-    plt.plot(interppointsx, interppointsy1, 'o', color=[0,0.3*(i-7),0])
-    plt.plot(interppointsx, interppointsy2, 'x', color=[0,0.3*(i-7),0])
-
-    for pindex in range(len(interppolys1)):
-        poly1 = interppolys1[pindex]
-        poly2 = interppolys2[pindex]
-        _xvals = np.linspace(interppointsx[pindex], interppointsx[pindex+1], 30)
-        xshift = np.linspace(-intervalsize[pindex], 0, 30)
-        y1 = poly1(xshift)
-        y2 = poly2(xshift)
-        plt.plot(_xvals, y1, '-.', color=[1, 1-.3*(i-7),0])
-        plt.plot(y1, y2, '-', color=[.3*(i-7), .3*(i-7), .3*(i-7)])
-        plt.plot(_xvals, y2, '-.', color=[0, .6, .3*(i-7)])
-
-plt.plot(xvals, yvals1, color=[1,0,0])
-plt.plot(xvals, yvals2, color=[0,0,1])
-plt.show()
+for n in range(1,20):
+    integral = trapez_integral(f, l, r, n)
+    print("Steps:", n, "\tValue:", integral, "\tError:", abs(ideal - integral))
+print("="*20)
+for n in range(1,20):
+    integral = romberg(f, l, r, n)[-1][-1]
+    print("Steps:", n, "\tValue:", integral, "\tError:", abs(ideal - integral))
